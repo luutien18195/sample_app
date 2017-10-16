c3f3f2910de1070b2f50f6dc92f7ca503bb0cb3f
luutien18195 <luutien18195@gmail.com>
2017-10-16 11:16:48 +0700

    Finish layout and routes

---
+++ db/schema.rb
@@ -0,0 +1,8 @@
+ActiveRecord::Schema.define(version: 20171012083311) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+end
