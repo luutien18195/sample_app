29ddc65630ff604784e1815e3095d88d838b0552
luutien18195 <luutien18195@gmail.com>
2017-10-17 11:13:03 +0700

    Modeling User

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
-  def home; end
-
-  def help; end
-
   def about; end
 
   def contact; end
+
+  def help; end
+
+  def home; end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,12 @@
-ActiveRecord::Schema.define(version: 20171012083311) do
+ActiveRecord::Schema.define(version: 20171017034448) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
