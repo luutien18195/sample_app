185dcf1993386b3d1c9daf6f0d8a98cc63d52a2f
luutien18195 <luutien18195@gmail.com>
2017-10-24 11:48:06 +0700

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,7 @@ module SessionsHelper
 
   def remember user
     user.remember
-
     cookies.permanent.signed[:user_id] = user.id
-
     cookies.permanent[:remember_token] = user.remember_token
   end
 
@@ -17,7 +15,7 @@ module SessionsHelper
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by id: user_id
 
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -40,4 +38,12 @@ module SessionsHelper
     cookies.delete :remember_token
   end
 
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save {email.downcase!}
+  before_create :create_activation_digest
 
   validates :name, presence: true,length: {maximum: Settings.max_length_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -13,21 +13,15 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.min_length_password}, allow_nil: true
 
-  scope :select_id_name_email, ->{select :id, :name, :email }
-  scope :order_created_at, ->{order :created_at }
+  scope :select_id_name_email, ->{select :id, :name, :email}
+  scope :order_created_at, ->{order :created_at}
 
-  class << self
-    def digest string
-      cost = ActiveModel::SecurePassword.min_cost ?
-        BCrypt::Engine::MIN_COST :
-        BCrypt::Engine.cost
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
 
-      BCrypt::Password.create string, cost: cost
-    end
-
-    def self.new_token
-      SecureRandom.urlsafe_base64
-    end
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   def current_user? user
@@ -39,12 +33,36 @@ class User < ApplicationRecord
     update_attributes remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false if remember_digest
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update_attribute :remember_digest, nil
+  end
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
